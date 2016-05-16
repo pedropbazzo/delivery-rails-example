119bffde5ad14915203b86db592cf097810e4d55
JJ Asghar <jj@chef.io>
2016-05-16 17:16:28 -0500

    WIP: trying to get the kitchen instance to converge

--- .delivery/build-cookbook/recipes/default.rb
+++ .delivery/build-cookbook/recipes/default.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+include_recipe 'delivery-truck::default'
+
+src_dir = File.expand_path("#{node['delivery']['workspace']['repo']}")
+
+execute "run bundler" do
+  command "bundle install"
+  creates "#{src_dir}/Gem.lock"
+
+  action :run
+end
